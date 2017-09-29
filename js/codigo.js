73b6ebe486e6e1c3c745d44638ccf99f0a1bfcde
warnerotz <adrichony@gmail.com>
2017-09-29 11:26:40 +0200

    29/09/2017 11:26

--- js/codigo.js
+++ js/codigo.js
@@ -1,18 +1,21 @@
 window.onload = function () {
-    //facil 9x9 y 10 minas
-    //intermedio 14x14 y 30 minas
-    //var minas
-    //var cantBanderas = 0;
-    //var perder = false;
-    //var ganar = false;
+    //variable que controla el nivel de dificultad
     var nivel = 0;
-    var filas = 0;
-    var columnas = 0;
-    var minas = 0;
+    //variable que controla la longitud del teclado
+    var longitud = 0;
+    //variable que controla el numero de bombas
+    var numBombas= 0;
+    //variable para la matriz con la que crearemos el tablero de juego.
+    var minas =[];
+    //variable que controla el numero de bombas acertadas
+    var numAciertos = 0;
+    //variable que controla el numero de espacios vacios descubiertos
+    var casillasNoBomba =0;
+    
     //listener para el nivel principiante que llama a la funcion crearTablero y le manda el numero de filas y columnas.
     document.getElementById("prin").addEventListener("click", iniciarJuego, false)
 
-
+    
     document.getElementById("inter").addEventListener("click", iniciarJuego, false)
 
     function iniciarJuego() {
@@ -32,20 +35,22 @@ window.onload = function () {
         alert("entro generador bombas");
         generadorBombas(minas);
     }
-
+//funcion que segun el nivel pone el tamaño del grid y el numero de bombas.
     function comprobarNivel() {
-
+        
         switch (nivel) {
-            case 1:
-                filas = 9;
-                columnas = 9;
+            case 1:                
+                longitud = 9;
+                numBombas = 10;
+                
                 break;
-            case 2:
-                filas = 14;
-                columnas = 14;
-
-
+            case 2:                
+                longitud = 10;
+                numBombas = 15
+                break;
         }
+        
+        casillasNoBomba =(longitud*longitud) - numBombas;
     }
 
 
@@ -56,16 +61,16 @@ window.onload = function () {
         var tabla = document.createElement("table");
         tabla.setAttribute("id", "tablaTablero");
         //bucle para introducir filas y columnas.
-        for (var i = 0; i < filas; i++) {
+        for (var i = 0; i < longitud; i++) {
             var fila = document.createElement("tr");
-            for (var j = 0; j < columnas; j++) {
+            for (var j = 0; j < longitud; j++) {
                 var columna = document.createElement("td");
                 var input = document.createElement('input');
                 input.setAttribute("type", "button");
                 input.classList.add("btn");
                 input.classList.add("btn-default");
                 input.classList.add("tamInputs");
-                input.setAttribute("id", i + "X" + j);
+                input.setAttribute("id", i+j);
                 //hacemos los listeners de los inputs
                 input.addEventListener("click", mostrarNumero, false);
                 //hacemos que el input cuelgue del td.
@@ -78,35 +83,32 @@ window.onload = function () {
 
         document.getElementById("tablero").appendChild(tabla);
     }
-
-    /*
-            function iniMatrizMinas() {
-                var tabla = Array();
-                for (var i = 0; i < filas; i++) {
-                    tabla[i] = [];
-                    for (var j = 0; i < columnas; j++) {
-                        tabla[i][j] = "0";
-                    }
-                }
-                return tabla;
-
-            }
-    */
+ /*  
     function iniMatrizMinas() {
 
         var matriz;
         for (var i = 0; i < filas; i++) {
             matriz[i] = [];
-        }
-
-        // ponemos el valor de cada celda de la matriz
-        for (var i = 0; i < filas; i++) {
             for (var j = 0; j < columnas; j++) {
                 matriz[i][j] = "0";
             }
         }
 
+    }
+   */
+    
+    function iniMatrizMinas(){
+          for (var i=0;i<longitud;i++) {
+        	    minas[i] = [];
+        	}
 
+        	// ponemos el valor de cada celda de la matriz
+        	for (var i=0;i<longitud;i++) {
+            	for (var j=0;j<longitud;j++) {
+                	minas[i][j] = " ";    
+            	}
+        	}
+        
     }
 
 
